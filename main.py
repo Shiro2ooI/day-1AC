c39c87d7cc30c9128a1e6007aa29383b2eadd42d
Adam <adam.kolacz01@gmail.com>
2025-05-20 21:28:54 +0200

    just a project

---
+++ main.py
@@ -0,0 +1,190 @@
+import getpass
+import os
+import operator
+import asyncio
+
+from typing import Annotated, List, Tuple, Union, Literal
+from typing_extensions import TypedDict
+
+from pydantic import BaseModel, Field
+
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+
+from langgraph.prebuilt import create_react_agent
+from langgraph.graph import END, StateGraph, START
+
+
+# <----------------------------------------------------------------------------------->
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+
+_set_env("OPENAI_API_KEY")
+_set_env("TAVILY_API_KEY")
+
+# <------------------------------------------------------------------------------------>
+
+tools = [TavilySearchResults(max_results=3)]
+
+
+llm = ChatOpenAI(model="gpt-4-turbo-preview")
+prompt = "You are a helpful assistant."
+agent_executor = create_react_agent(llm, tools, prompt=prompt)
+
+class PlanExecute(TypedDict):
+    input: str
+    plan: List[str]
+    past_steps: Annotated[List[Tuple], operator.add]
+    response: str
+
+class Plan(BaseModel):
+    """Plan to follow in future"""
+
+    steps: List[str] = Field(
+        description="different steps to follow, should be in sorted order"
+    )
+
+planner_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """For the given objective, come up with a simple step by step plan. \
+                This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+                The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.""",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
+planner = planner_prompt | ChatOpenAI(
+    model="gpt-4o", temperature=0
+).with_structured_output(Plan)
+
+class Response(BaseModel):
+    """Response to user."""
+    response: str
+
+
+class Act(BaseModel):
+    """Action to perform."""
+
+    action: Union[Response, Plan] = Field(
+        description="Action to perform. If you want to respond to user, use Response. "
+        "If you need to further use tools to get the answer, use Plan."
+    )
+
+
+replanner_prompt = ChatPromptTemplate.from_template(
+    """For the given objective, come up with a simple step by step plan. \
+This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
+The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+
+Your objective was this:
+{input}
+
+Your original plan was this:
+{plan}
+
+You have currently done the follow steps:
+{past_steps}
+
+Update your plan accordingly. If no more steps are needed and you can return to the user, then respond with that. Otherwise, fill out the plan. Only add steps to the plan that still NEED to be done. Do not return previously done steps as part of the plan."""
+)
+
+
+replanner = replanner_prompt | ChatOpenAI(
+    model="gpt-4o", temperature=0
+).with_structured_output(Act)
+
+async def execute_step(state: PlanExecute):
+    plan = state["plan"]
+    plan_str = "\n".join(f"{i+1}. {step}" for i, step in enumerate(plan))
+    task = plan[0]
+    task_formatted = f"""For the following plan:
+{plan_str}\n\nYou are tasked with executing step {1}, {task}."""
+    agent_response = await agent_executor.ainvoke(
+        {"messages": [("user", task_formatted)]}
+    )
+    return {
+        "past_steps": [(task, agent_response["messages"][-1].content)],
+    }
+
+
+async def plan_step(state: PlanExecute):
+    plan = await planner.ainvoke({"messages": [("user", state["input"])]})
+    return {"plan": plan.steps}
+
+
+async def replan_step(state: PlanExecute):
+    output = await replanner.ainvoke(state)
+    print("\n🔁 Replan step output:\n", output)
+    if isinstance(output.action, Response):
+        return {"response": output.action.response}
+    else:
+        return {"plan": output.action.steps}
+
+
+def should_end(state: PlanExecute):
+    print("🧪 Checking end condition. Current state keys:", state.keys())
+    if "response" in state and state["response"]:
+        print("✅ Ending.")
+        return END
+    else:
+        print("↪️ Continuing.")
+        return "agent"
+workflow = StateGraph(PlanExecute)
+
+workflow.add_node("planner", plan_step)
+
+workflow.add_node("agent", execute_step)
+
+workflow.add_node("replan", replan_step)
+
+workflow.add_edge(START, "planner")
+
+workflow.add_edge("planner", "agent")
+
+workflow.add_edge("agent", "replan")
+
+workflow.add_conditional_edges(
+    "replan",
+    should_end,
+    ["agent", END],
+)
+
+
+app = workflow.compile()
+
+config = {"recursion_limit": 50}
+inputs = {"input": "what is the hometown of the mens 2024 Australia open winner?"}
+
+
+def chat():
+    print(" Agent ready! what should i plan and execute? Type 'exit' to quit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit"]:
+            print("👋 Goodbye!")
+            break
+        asyncio.run(run_agent(user_input))
+        
+
+
+MAX_STEPS = 10
+
+async def run_agent(user_input: str):
+    state = None
+    for i in range(MAX_STEPS):
+        print(f"\n Step {i+1}")
+        state = await app.ainvoke({"input": user_input})
+        if "response" in state and state["response"]:
+            break
+    if state and "response" in state:
+        print(" Bot:", state["response"])
+    else:
+        print(" Bot couldn't complete the task within step limit.")
+
+chat()
